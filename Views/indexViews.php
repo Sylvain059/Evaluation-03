8846c407103015f93f24ae5cb7f5faee456d253d
Sylvain059 <sylvain.sculier88@gmail.com>
2017-11-29 16:33:15 +0100

    indexViews.php // add Bootstrap card

--- Views/indexViews.php
+++ Views/indexViews.php
@@ -1,12 +1,10 @@
 <?php require 'Templates/head.php'; ?>
 <main class="container">
     <div class="card" style="width: 20rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
         <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h4 class="card-title"></h4>
+            <p class="card-text"></p>
+            <a href="#" class="btn btn-primary">See More</a>
         </div>
     </div>
 
