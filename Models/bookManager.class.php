03bed5a1c0936bccb847ea15c318865956428d23
Sylvain059 <sylvain.sculier88@gmail.com>
2017-12-03 22:16:35 +0100

    UserManager & BookManagr // class create

---
+++ Models/bookManager.class.php
@@ -0,0 +1,22 @@
+<?php
+
+class BookManager
+{
+    // Add book
+    public function addBook()
+    {
+
+    }
+
+    // Select book
+    public function getBook()
+    {
+
+    }
+
+    // Update book
+    public function updateBook()
+    {
+
+    }
+}
